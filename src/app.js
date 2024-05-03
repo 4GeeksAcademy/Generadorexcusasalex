17a799aa12cdd9ea2e9cff77524ff3433f3b1e0c
alexfulldev <166410130+alexfulldev@users.noreply.github.com>
2024-05-03 19:34:25 +0000

    proyecto acabado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#Excuse").innerHTML = excuseGenerator();
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+/** 1-Index.html
+ * 2-Crear variables
+ * 3-Función Random
+ * 4-Modificar HTML
+ */
+
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const whoRandom = Math.floor(Math.random() * who.length);
+  const actionRandom = Math.floor(Math.random() * action.length);
+  const whatRandom = Math.floor(Math.random() * what.length);
+  const whenRandom = Math.floor(Math.random() * when.length);
+
+  const excuse =
+    who[whoRandom] + action[actionRandom] + what[whatRandom] + when[whenRandom];
+  return excuse;
+}
